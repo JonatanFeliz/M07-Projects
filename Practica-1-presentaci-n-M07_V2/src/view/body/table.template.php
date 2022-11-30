142d65dad37866a8dd346d168316eb74530ecf51
Pmartinez03 <74315493+Pmartinez03@users.noreply.github.com>
2022-11-30 13:05:36 +0100

    Add files via upload

---
+++ Practica-1-presentaci-n-M07_V2/src/view/body/table.template.php
@@ -0,0 +1,12 @@
+<h1> Clasificacion de LaLiga </h1>
+
+<table>
+
+     <?php
+     require_once(__DIR__ . '/../viewlib.php');
+
+     echo View\get_html_header($football_table->header);
+     echo View\get_html_body($football_table->body);
+     ?>
+
+</table>
