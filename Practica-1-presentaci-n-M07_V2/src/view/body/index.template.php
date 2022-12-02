47e11e2565c91070e875bbda657765502c67dcf7
anthonyy2711 <amreyesnarvaez@gmail.com>
2022-12-02 13:31:38 +0100

    imgs, index.template, skeleton.template

--- Practica-1-presentaci-n-M07_V2/src/view/body/index.template.php
+++ Practica-1-presentaci-n-M07_V2/src/view/body/index.template.php
@@ -1,10 +1,82 @@
-<h1> Welcome to my WebApp! </h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="/img/logo.png" alt="" width="60" height="60">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="/blog">BLOG</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="/gallery">GALERIA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="/table">TABLA DE DATOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="/web-service">WEB SERVICE</a>
+        </li>
 
-<ol>
-    <li> <a href="/blog">       Blog        </a>    </li>
-    <li> <a href="/gallery">    Gallery     </a>    </li>
-    <li> <a href="/table">       Table        </a>    </li>
-    <li> <a href="/web-service">Web Service </a>    </li>
-</ol>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<br>
+<div class="row">
+    <div class="col-md-3 col-sm-6">
+        <div class="card" style="width: 20rem; height: 25rem;">
+            <img src="img/blog.jpeg" class="card-img-top" alt="..." height="200rem">
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text text-dark">Noticias diarias del mundo del futbol.<br><br>Aqui se apoya al FC Barcelona.</p>
+                <a href="blog" class="btn btn-info">Blog</a>
+            </div>        
+        </div>
+    </div>
 
-<img src="/img/welcome.jpg" alt="Test image">
+    <div class="col-md-3 col-sm-6" >
+        <div class="card" style="width: 20rem; height: 25rem;">
+            <img src="img/galeria.jpeg" class="card-img-top" alt="..." height="200rem">
+            <div class="card-body">
+                <h5 class="card-title">Galeria de fotos</h5>
+                <p class="card-text text-dark">Álbumes de fotos con los ganadores de las ultimas competiciones Champions League.</p>
+                <a href="gallery" class="btn btn-info">Galeria</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6" >
+        <div class="card" style="width: 20rem; height: 25rem;">
+            <img src="img/data-table.jpeg" class="card-img-top" alt="..." height="200rem">
+            <div class="card-body">
+                <h5 class="card-title">Tabla de datos</h5>
+                <p class="card-text text-dark">Tabla HTML con datos que se leen de un fichero .csv y mostrarlas</p>
+                <a href="table" class="btn btn-info">Tabla</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6" >
+        <div class="card" style="width: 20rem; height: 25rem;">
+            <img src="img/web-service.jpeg" class="card-img-top" alt="..." height="200rem">
+            <div class="card-body">
+                <h5 class="card-title">Web Service</h5>
+                <p class="card-text text-dark">Servicio con información sobre el mundo del futbol.</p>
+                <a href="web-service" class="btn btn-info">Web Service</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+        <p id="iden">PARJ | Jonatan Feliz - Anthony Reyes - Pau Martinez - Ruyou Hu Ye</p>
